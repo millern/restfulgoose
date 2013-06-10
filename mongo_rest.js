d03ace686a162feff2b5e1a92d7d2be271530f52
Nick Miller <njmiller6@gmail.com>
2013-06-10 10:31:38 -0700

    GET by id

--- mongo_rest.js
+++ mongo_rest.js
@@ -18,41 +18,41 @@ module.exports = function(){
     var routes = {
       'GET': [
         {
-         root: function(collection){
-           console.log('find all in collection');
-           client.open(function(err,p_client){
-             client.collection(collection,function(err,collection){
-               collection.find().toArray(function(err,results){
-                 res.end(JSON.stringify(results));
-                 client.close();
-               });
-             });
-           });
-         },
-         id: function(collection, query){
-           console.log('find by id');
-           client.open(function(err,p_client){
-             client.collection(collection,function(err,collection){
-               collection.find({_id: query }).toArray(function(err,results){
-                 res.end(JSON.stringify(results));
-                 client.close();
-               });
-             });
-           });
-         },
-         query: function(collection, query){
-           console.log('query');
-           res.end(JSON.stringify("queried by query"));
-           client.close();
-         }
+          root: function(collection){
+            console.log('find all in collection');
+            client.open(function(err,p_client){
+              client.collection(collection,function(err,collection){
+                collection.find().toArray(function(err,results){
+                  res.end(JSON.stringify(results));
+                  client.close();
+                });
+              });
+            });
+           },
+          id: function(collection, query){
+            console.log('finding by id: ' + query);
+            client.open(function(err,p_client){
+              client.collection(collection,function(err,collection){
+                collection.findOne({_id: new BSON.ObjectID(query)}, function(err, item) {
+                  res.end(JSON.stringify(item));
+                  client.close();
+                });
+              });
+            });
+           },
+          query: function(collection, query){
+            console.log('query');
+            res.end(JSON.stringify("queried by query"));
+            client.close();
+          }
         }
       ],
-      'POST': []
+      'POST': [
+      ]
     };
     var trailblazer = function(collection, id, method){
-      console.log("BSON", BSON.ObjectID(id));
       if (id){
-        routes[method][0].id.apply(null,[collection, BSON.ObjectID(id)]);
+        routes[method][0].id.apply(null,[collection, id]);
       } else {
         routes[method][0].root.apply(null,[collection]);
       }
